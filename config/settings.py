e7b8dc58ea23deb700e94148faddbe51fa0c80a8
202210795-lab <TU_CORREO_DE_GITHUB>
2026-08-01 19:02:11 -0400

    arreglar archivos static e imagenes

--- config/settings.py
+++ config/settings.py
@@ -4,6 +4,7 @@ Django settings for config project.
 
 from pathlib import Path
 
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -11,85 +12,149 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-o$v#h0y5ee#n*3i0l5(l6-&e7l^_(aq+qksw^4^hw4h&8u93mn'
 
 
-# Producción Render
+# Render / Producción
+
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
 
+
 # Application definition
 
 INSTALLED_APPS = [
+
     'django.contrib.admin',
+
     'django.contrib.auth',
+
     'django.contrib.contenttypes',
+
     'django.contrib.sessions',
+
     'django.contrib.messages',
+
     'django.contrib.staticfiles',
 
+
     'estudiantes',
+
 ]
+
 
 
 MIDDLEWARE = [
+
     'django.middleware.security.SecurityMiddleware',
+
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
     'django.contrib.sessions.middleware.SessionMiddleware',
+
     'django.middleware.common.CommonMiddleware',
+
     'django.middleware.csrf.CsrfViewMiddleware',
+
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+
     'django.contrib.messages.middleware.MessageMiddleware',
+
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
 ]
+
 
 
 ROOT_URLCONF = 'config.urls'
 
 
+
 TEMPLATES = [
+
     {
+
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
+
         'DIRS': [BASE_DIR / 'templates'],
+
         'APP_DIRS': True,
+
         'OPTIONS': {
+
             'context_processors': [
+
                 'django.template.context_processors.request',
+
                 'django.contrib.auth.context_processors.auth',
+
                 'django.contrib.messages.context_processors.messages',
+
             ],
+
         },
+
     },
+
 ]
+
 
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
 
+
+
+
 # Database
 
 DATABASES = {
+
     'default': {
+
         'ENGINE': 'django.db.backends.sqlite3',
+
         'NAME': BASE_DIR / 'db.sqlite3',
+
     }
+
 }
+
+
+
 
 
 # Password validation
 
 AUTH_PASSWORD_VALIDATORS = [
+
     {
+
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+
     },
+
     {
+
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+
     },
+
     {
+
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+
     },
+
     {
+
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+
     },
+
 ]
+
+
+
 
 
 # Internationalization
@@ -104,16 +169,35 @@ USE_TZ = True
 
 
 
-# Static files
+
+
+# Archivos estáticos (CSS, imágenes, JS)
 
 STATIC_URL = '/static/'
+
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
 STATICFILES_DIRS = [
+
     BASE_DIR / 'estudiantes' / 'static',
+
 ]
 
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+
+
+
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+
+
+# Login
+
+LOGIN_URL = '/login/'
